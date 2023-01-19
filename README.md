1537743b9a53563de860de97163869614af6d6f1
urhitech <admin@urhitech.com>
2023-01-19 11:16:56 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# Urhitech SMS API SDK
+
+The Urhitech SMS API SDK provides a suitable approach to the USMSGH API from applications written in PHP. It includes pre-defined set of classes and functions for API resource that initialize themeselves from  API responses.
+
+The library provides other features. For Example:
+1. Easy configuration path for fast setup and use
+2. Helpers for pagination.
+
+You can sign up for a USMSGH account at [usmsgh.com](https://www.usmsgh.com)
+
+## Prerequisites
+PHP 5.6.0  and later
+
+## Installation
+Via [Composer](http://getcomposer.org/)
+```
+$ composer require supreme/urhitech-usmsgh
+```
+
+Via Git Bash
+```
+git clone https://github.com/urhitech/urhitech-smsgh-api.git
+```
+
+## Documentation
+Please see [https://usmsgh.com/developer/](https://usmsgh.com/developer/) for up-to-date documentation
+
+## Usage
+
+### Step 1:
+If you install the Urhitech SMS API SDK via Git Clone then load the Urhitech SMS PHP API class file and use namespace.
+
+```php
+require_once '/path/to/src/Usmsgh.php';
+use Urhitec\Usms;
+```
+
+If you install Urhitech SMS API SDK via [Composer](http://getcomposer.org/) the Require the autoload.php file in the index.php of your project or whatever file you need to use Urhitech SMS PHP API classes.
+
+```php
+require __DIR__ . '/vendor/autoload.php';
+use use Urhitec\Usms;;
+```
+
+The Urhitech SMS API SDK endpoints are RESTful, and consume and return JSON. All Http endpoints requires an API Key in the request header.
+
+For more information on how to get an API Key visit [here](https://webapp.usmsgh.com/developers) to copy or generate new key for authorization.
+
+## HTTP ENDPOINTS
+* https://webapp.usmsgh.com/api/sms/send
+* https://webapp.usmsgh.com/api/sms/{uid}
+* https://webapp.usmsgh.com/api/sms
+* https://webapp.usmsgh.com/api/me
+* https://webapp.usmsgh.com/api/balance
+* https://webapp.usmsgh.com/api/contacts
+* https://webapp.usmsgh.com/api/contacts/{group_id}/show/
+* https://webapp.usmsgh.com/api/contacts/{group_id}
+* https://webapp.usmsgh.com/api/contacts/{group_id}/store
+* https://webapp.usmsgh.com/api/contacts/{group_id}/search/{uid}
+* https://webapp.usmsgh.com/api/contacts/{group_id}/update/{uid}
+* https://webapp.usmsgh.com/api/contacts/{group_id}/delete/{uid}
+* https://webapp.usmsgh.com/api/contacts/{group_id}/all
+
+
+### Step 2:
+Instantiate the Urhitech SMS PHP API
+```php
+$client = new Urhitec\Usms;
+```
+
+## Send SMS
+```php
+$api_key = "Enter Your API Key here";
+
+$url = "https://webapp.usmsgh.com/api/sms/send";
+
+$recipients = "233500000000,233540000000";
+$message = "Hello world";
+$senderid = "Enter your approved sender ID here";
+
+$response = $client->sendSMS($url, $api_key, $senderid, $recipients, $message);
+```
+
+
+## Check SMS Credit Balance
+```php
+$api_key = "Enter Your API Key here";
+
+$url = "https://webapp.usmsgh.com/api/balance";
+
+$get_credit_balance = $client->check_balance($url, $api_key);
+```
+
+
+## View Profile
+```php
+$api_key = "Enter Your API Key here";
+
+$url = "https://webapp.usmsgh.com/api/me";
+
+$get_profile = $client->profile($url, $api_key);
+```
---
+++ src/UsmsghAPI.php
@@ -0,0 +1,301 @@
+<?php
+
+namespace Urhitech\UsmsghAPI;
+
+class UsmsghAPI
+{
+    static $curl_handle = NULL;
+    /**
+     * @param $request_method
+     * @param $url
+     * @param $api_token
+     * @param $post_fields
+     * @return mixed
+     *
+     * Send Request to server and get sms status
+     */
+
+    private function send_server_response($endpoint, $api_token, $sender_id, $recipient, $message, $request_method = null)
+    {
+        //Initialize the curl handle if it is not initialized yet
+        if (!isset($this::$curl_handle)) {
+            $this::$curl_handle = curl_init();
+        }
+
+        $data = json_encode([
+            'recipient' => $recipient,
+            'sender_id' => $sender_id,
+            'message' => $message,
+        ]);
+
+        curl_setopt ($this::$curl_handle, CURLOPT_URL, $endpoint);
+        if ($request_method == 'post') {
+            curl_setopt ($this::$curl_handle, CURLOPT_POST, true);
+            curl_setopt ($this::$curl_handle, CURLOPT_POSTFIELDS, $data);
+        }
+
+        // request_method == PUT
+        if ($request_method == 'put') {
+            curl_setopt($this::$curl_handle, CURLOPT_CUSTOMREQUEST, 'PUT');
+            curl_setopt($this::$curl_handle, CURLOPT_POSTFIELDS, $data);
+        }
+
+        // request_method == PATCH
+        if ($request_method == 'patch') {
+            curl_setopt($this::$curl_handle, CURLOPT_CUSTOMREQUEST, 'PATCH');
+            curl_setopt($this::$curl_handle, CURLOPT_POSTFIELDS, $data);
+        }
+
+        // request_method == DELETE
+        if ($request_method == 'delete') {
+            curl_setopt($this::$curl_handle, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        }
+
+        curl_setopt ($this::$curl_handle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt ($this::$curl_handle, CURLOPT_HTTPHEADER, [
+            "accept: application/json",
+            "authorization: Bearer ".$api_token
+        ]);
+
+        // Allow cURL function to execute 20sec
+        curl_setopt($this::$curl_handle, CURLOPT_TIMEOUT, 20);
+
+        // waiting 20 secs while waiting to connect
+        curl_setopt($this::$curl_handle, CURLOPT_CONNECTTIMEOUT, 20);
+
+        if ($e = curl_error($this::$curl_handle)) {
+            return $e;
+        } else {
+            return json_decode(curl_exec( $this::$curl_handle ), true);
+            curl_close($this::$curl_handle);
+        }
+
+    }
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @param $post_fields
+     * @return mixed
+     *
+     * Send single / group SMS
+     */
+    public function send_sms($endpoint, $api_token, $sender_id, $phones, $message)
+    {
+        $single_phone = $phones;
+        $phones = explode(',', $phones);
+        if (count($phones) > 1) {
+            foreach ($phones as $phone) {
+                $this->send_server_response($endpoint, $api_token, $sender_id, $phone, $message, 'post');
+            }
+        } else {
+            // print_r($single_phone);exit();
+            $this->send_server_response($endpoint, $api_token, $sender_id, $single_phone, $message, 'post');
+        }
+
+        return false;
+    }
+
+
+
+
+    /**
+     * @param $endpoint
+     * @param $api_token
+     * @param $sender_id
+     * @param $phone
+     * @param $message
+     * @return mixed
+     *
+     * Send single
+     */
+    public function sendSingle($endpoint, $api_token, $sender_id, $phone, $message)
+    {
+        $this->send_server_response($endpoint, $api_token, $sender_id, $phone, $message, 'post');
+    }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @return mixed
+     *
+     * View an SMS
+     */
+    public function view_sms($url, $api_token)
+    {
+        return $this->send_server_response($url, $api_token, '','', '');
+    }
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @return mixed
+     *
+     * View profile
+     */
+    public function profile($url, $api_token)
+    {
+        return $this->send_server_response($url, $api_token, '', '', '');
+    }
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @return mixed
+     *
+     * View sms credit balance
+     */
+    public function check_balance($url, $api_token)
+    {
+
+        return $this->send_server_response($url, $api_token, '', '', '');
+    }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @param $post_fields => ['name' => 'your group name']
+     * @return mixed
+     *
+     * Create a new Contact Group
+     */
+    public function create_contact_group($endpoint, $api_token, $sender_id, $phones, $message)
+    {
+        return $this->send_server_response($endpoint, $api_token, $sender_id, $phones, $message, 'post');
+    }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @return mixed
+     *
+     * View Contact Group
+     */
+    public function view_contact_group($url, $api_token)
+    {
+        return $this->send_server_response($url, $api_token, '', '', 'post');
+    }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @param $post_fields
+     * @return mixed
+     *
+     * Update Contact Group
+     */
+    public function update_contact_group($endpoint, $api_token, $sender_id, $phones, $message)
+    {
+        return $this->send_server_response($endpoint, $api_token, $sender_id, $phones, $message, 'patch');
+    }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @return mixed
+     *
+     * Delete Contact Group
+     */
+    public function delete_contact_group($url, $api_token)
+    {
+        return $this->send_server_response($url, $api_token, '', '', 'delete');
+    }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @return mixed
+     *
+     * View all Contact Groups
+     */
+    public function all_contact_groups($url, $api_token)
+    {
+        return $this->send_server_response($url, $api_token, '', '', '');
+    }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @param $post_fields
+     * @return mixed
+     *
+     * Creates a new contact object
+     */
+    public function create_contact($endpoint, $api_token, $sender_id, $phones, $message)
+    {
+        return $this->send_server_response($endpoint, $api_token, $sender_id, $phones, $message, 'post');
+    }
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @return mixed
+     *
+     * Retrieves the information of an existing contact
+     */
+    public function view_contact($url, $api_token)
+    {
+        return $this->send_server_response($url, $api_token, '', '', 'post');
+    }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @param $post_fields
+     * @return mixed
+     *
+     * Update an existing contact.
+     */
+    public function update_contact($endpoint, $api_token, $sender_id, $phones, $message)
+    {
+        return $this->send_server_response($endpoint, $api_token, $sender_id, $phones, $message, 'patch');
+    }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @return mixed
+     *
+     * Delete an existing contact
+     */
+    public function delete_contact($url, $api_token)
+    {
+        return $this->send_server_response($url, $api_token, '', '', 'delete');
+    }
+
+
+
+    /**
+     * @param $url
+     * @param $api_token
+     * @return mixed
+     *
+     * View all contacts in group
+     */
+    public function all_contacts_in_group($url, $api_token)
+    {
+        return $this->send_server_response($url, $api_token, '', '', 'post');
+    }
+}
